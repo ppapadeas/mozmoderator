0938234c1bbddb1a8ba17fec18a7a08fc50b910a
Pierros Papadeas <ppapadeas@gmail.com>
2014-03-12 09:03:14 -0700

    Wording and copy changes for productivity instance

--- moderator/moderate/forms.py
+++ moderator/moderate/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from models import Question
 
 
-Q_PLACEHOLDER = 'Ask your question in 250 characters'
+Q_PLACEHOLDER = 'What are the things at Mozilla that keep you from getting work done? (max 250 characters)'
 
 
 class QuestionForm(forms.ModelForm):
